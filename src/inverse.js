b8d1edfa03a42aefa5c9f516e9627d6f17d5dd5c
AK <anttikup@metropolia.fi>
2023-03-17 10:32:29 +0200

    Functions to get inverse

---
+++ src/inverse.js
@@ -0,0 +1,37 @@
+import { operators, functions } from './expr/data.js';
+
+export function getInverse(op_or_func) {
+    switch ( op_or_func ) {
+        case '+/1':
+            return '-/1';
+        case 'sin/1':
+            return 'arcsin/1';
+        case 'cos/1':
+            return 'arccos/1';
+        case 'tan/1':
+            return 'arctan/1';
+
+        case '+/2':
+            return '-/2';
+        case '·/2':
+            return ':/2';
+        case '^/2':
+            return '√/2';
+    }
+
+    throw new Error(`Unknown function ${op_or_func} or not the main function`);
+}
+
+export function getReverse(op_or_func) {
+    switch ( op_or_func ) {
+        case '^/2':
+            return 'log/2';
+    }
+
+    const inv = getInverse(op_or_func);
+    if ( inv ) {
+        return inv;
+    }
+
+    throw new Error(`Unknown function ${op_or_func} or not the main function`);
+}
