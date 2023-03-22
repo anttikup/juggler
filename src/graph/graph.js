f274c9c7762c7685f4940bffad6ffcf50e31ba95
AK <anttikup@metropolia.fi>
2023-03-22 07:02:20 +0200

    Encapsulated Network to Graph object

---
+++ src/graph/graph.js
@@ -0,0 +1,38 @@
+import { Network } from "vis-network";
+
+import { rpnToGraph } from './exprToGraph.js';
+import { graphToRPN } from './graphToExpr.js';
+
+export default class Graph {
+
+    constructor(container) {
+        const options = {};
+        const data = {};
+        this.vis = new Network(container, data, options);
+    }
+
+    on(event, func) {
+        this.vis.on('click', func);
+    }
+
+    getSelectedNodes() {
+        return this.vis.getSelectedNodes();
+    }
+
+    setRPN(rpn) {
+        this.vis.setData(rpnToGraph(rpn));
+    }
+
+    getRPN(povId) {
+        return graphToRPN(this.vis, povId);
+    }
+
+    get nodes() {
+        return this.vis.body.data.nodes;
+    }
+
+    get edges() {
+        return this.vis.body.data.edges;
+    }
+
+};
